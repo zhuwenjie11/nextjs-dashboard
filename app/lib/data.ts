89a97a8277e9dad2097af2b99156684e808d7ee3
zhuwenjie <zhuwenjie@eastcom-sw.com>
2025-05-14 20:42:20 +0800

    modified:   app/lib/data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,7 +19,7 @@ export async function fetchRevenue() {
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue[]>`SELECT * FROM revenue`;
+    const data = await sql<Revenue[]>`SELECT1 * FROM revenue`;
 
     console.log('...Data fetch completed after 3 seconds.');
 
