1c749acb0d6f488f5c285aa130a2c7abf232bc40
zhuwenjie <zhuwenjie@eastcom-sw.com>
2025-05-15 09:06:33 +0800

    modified:   app/lib/data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,7 +21,7 @@ export async function fetchRevenue() {
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    console.log('...    Data fetch completed after 3 seconds.');
+    console.log('sleep Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
